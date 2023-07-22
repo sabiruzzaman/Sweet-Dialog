5228727ec26da2b4ec5452f165bdec77828b5a2b
Sabuj <sabuj.syntech@gmail.com>
2023-07-22 14:53:28 +0600

    screenshot add

--- app/src/main/java/com/example/sweetdialog/MainActivity.java
+++ app/src/main/java/com/example/sweetdialog/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.sweetdialog;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.sbz.sweetdialog.SweetDialog;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +14,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView textView = findViewById(R.id.clickTvId);
+        textView.setOnClickListener(v -> new SweetDialog.Builder(MainActivity.this)
+                .setImage(getDrawable(R.drawable.no_internet))
+                .setTitle("Error!!")
+                .setBody("No Internet. Please try again!!")
+                .setPositiveBtnTxt("Retry")
+                .setPositiveListener(() -> {
+                    // TODO add your positive logic here
+                })
+                .setNegativeListener(() -> {
+                    // TODO add your negative logic here
+                })
+                .setCancel(true)
+                .build());
 
 
     }
